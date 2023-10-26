efb15336e423d29200f95620815b5cf700ffecdd
VictoriaPresnyakova <victoriapresnyakova202@gmail.com>
2023-10-26 16:35:37 +0400

    businessLogic done

---
+++ BusinessLogics/EducationLogic.cs
@@ -0,0 +1,65 @@
+﻿using StudentContracts.BindingModels;
+using StudentContracts.BusinessLogicContracts;
+using StudentContracts.StorageContracts;
+using StudentContracts.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogics
+{
+    public class EducationLogic : IEducationLogic
+    {
+        private readonly IEducationStorage _educationStorage;
+        public EducationLogic(IEducationStorage educationStorage)
+        {
+            _educationStorage = educationStorage;
+        }
+
+        public void CreateOrUpdate(EducationBindingModel model)
+        {
+            var element = _educationStorage.GetElement(
+               new EducationBindingModel
+               {
+                   Name = model.Name
+               });
+            if (element != null && element.Id != model.Id)
+            {
+                throw new Exception("Такая программа уже существует");
+            }
+            if (model.Id.HasValue)
+            {
+                _educationStorage.Update(model);
+            }
+            else
+            {
+                _educationStorage.Insert(model);
+            }
+        }
+
+        public void Delete(EducationBindingModel model)
+        {
+            var element = _educationStorage.GetElement(new EducationBindingModel { Id = model.Id });
+            if (element == null)
+            {
+                throw new Exception("Программа не найдена");
+            }
+            _educationStorage.Delete(model);
+        }
+
+        public List<EducationViewModel> Read(EducationBindingModel model)
+        {
+            if (model == null)
+            {
+                return _educationStorage.GetFullList();
+            }
+            if (model.Id.HasValue)
+            {
+                return new List<EducationViewModel> { _educationStorage.GetElement(model) };
+            }
+            return _educationStorage.GetFilteredList(model);
+        }
+    }
+}
---
+++ BusinessLogics/StudentLogic.cs
@@ -0,0 +1,67 @@
+﻿using StudentContracts.BindingModels;
+using StudentContracts.BusinessLogicContracts;
+using StudentContracts.StorageContracts;
+using StudentContracts.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogics
+{
+    public class StudentLogic : IStudentLogic
+    {
+        private readonly IStudentStorage _studentStorage;
+        public StudentLogic(IStudentStorage studentStorage)
+        {
+            _studentStorage = studentStorage;
+        }
+        public void CreateOrUpdate(StudentBindingModel model)
+        {
+            var element = _studentStorage.GetElement(
+               new StudentBindingModel
+               {
+                   Name = model.Name,
+                   Av_Score = model.Av_Score,
+                   Education_Form = model.Education_Form,
+                   Date = model.Date,
+               });
+            if (element != null && element.Id != model.Id)
+            {
+                throw new Exception("Студент с таким именем уже существует");
+            }
+            if (model.Id.HasValue)
+            {
+                _studentStorage.Update(model);
+            }
+            else
+            {
+                _studentStorage.Insert(model);
+            }
+        }
+
+        public void Delete(StudentBindingModel model)
+        {
+            var element = _studentStorage.GetElement(new StudentBindingModel { Id = model.Id });
+            if (element == null)
+            {
+                throw new Exception("Студент не найден");
+            }
+            _studentStorage.Delete(model);
+        }
+
+        public List<StudentViewModel> Read(StudentBindingModel model)
+        {
+            if (model == null)
+            {
+                return _studentStorage.GetFullList();
+            }
+            if (model.Id.HasValue)
+            {
+                return new List<StudentViewModel> { _studentStorage.GetElement(model) };
+            }
+            return _studentStorage.GetFilteredList(model);
+        }
+    }
+}
